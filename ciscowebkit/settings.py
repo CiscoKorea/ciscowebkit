4d5c4271867653a588161bbf1a52d6883b1babce
Hye-Churn, Jang <hyjang@cisco.com>
2016-07-28 20:14:57 +0900

    Commit

--- ciscowebkit/settings.py
+++ ciscowebkit/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
