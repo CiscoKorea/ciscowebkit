f38c4e8fa884d72daf4274efd8f288cf2e6c1a18
Hye-Churn, Jang <hyjang@cisco.com>
2016-07-28 20:22:37 +0900

    Disable CSRF

--- ciscowebkit/settings.py
+++ ciscowebkit/settings.py
@@ -57,11 +57,11 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ciscowebkit.views.DisableCSRF',
 ]
 
 ROOT_URLCONF = 'ciscowebkit.urls'
--- ciscowebkit/views.py
+++ ciscowebkit/views.py
@@ -10,3 +10,7 @@ from ciscowebkit.common.engine import Engine
 
 def action(request):
     return Engine.GET().__action__(request)
+
+class DisableCSRF(object):
+    def process_request(self, request):
+        setattr(request, '_dont_enforce_csrf_checks', True)
