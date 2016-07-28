50081228c791b910ab90a0a0c1490624be2a9f77
Hye-Churn, Jang <hyjang@cisco.com>
2016-07-28 20:38:57 +0900

    Ascii

--- product/ACI/feature/Setting.py
+++ product/ACI/feature/Setting.py
@@ -39,11 +39,11 @@ class Setting(Feature):
     
     def post(self, request, data, *cmd):
         apic = APIC.addDomain(data.domain, data.ips, data.user, data.pwd)
-        if apic: self.info = InfoBlock('연결성공', '%s의 APIC과 %s로 연결되었습니다.' % (apic.domain, apic.connected)) 
+        if apic: self.info = InfoBlock('연결성공', u'%s의 APIC과 %s로 연결되었습니다.' % (apic.domain, apic.connected)) 
         else: self.info = InfoBlock('연결실패', 'APIC 연결이 실패하였습니다. 연결정보를 확인하세요.')
         return self.get(request, *cmd)
     
     def delete(self, request, data, *cmd):
         APIC.delDomain(data)
-        self.info = InfoBlock('연결삭제', '%s의 연결을 제거하였습니다/' % data)
+        self.info = InfoBlock('연결삭제', '%s의 연결을 제거하였습니다.' % data)
         return self.get(request, *cmd)
