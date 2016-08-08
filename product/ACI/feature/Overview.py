a564092994369da1af2c46f396599053e9e6d1d2
Hye-Churn, Jang <hyjang@cisco.com>
2016-08-08 17:37:31 +0900

    Overview tick

--- product/ACI/feature/Overview.py
+++ product/ACI/feature/Overview.py
@@ -12,7 +12,7 @@ from ciscowebkit.common import *
 class Overview(Feature):
      
     def __init__(self):
-        Feature.__init__(self, 3, 'fa-dashboard')
+        Feature.__init__(self, 10, 'fa-dashboard')
      
     def get(self, request, *cmd):
         if len(APIC) == 0: return InfoBlock('데이터 없음', '연결된 APIC이 없습니다. Setting 메뉴에서 APIC 연결을 추가하세요.')
