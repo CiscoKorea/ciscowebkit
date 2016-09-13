61b0d325f9626860bdfab7e4d9b2dcf0ca3208a0
Hye-Churn, Jang <hyjang@cisco.com>
2016-09-13 18:11:27 +0900

    docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-docker pull mariadb
+echo "Start CWDB Container"
+docker run --name cwdb -e MYSQL_ROOT_PASSWORD=cisco123 -d mariadb
 
-docker run -d --name cwdb -e MYSQL_ROOT_PASSWORD=cisco123 -d mariadb
+while true; do
+        echo "Wait to start CWDB"
+        docker run -it --link cwdb:mysql --rm mariadb sh -c 'exec mysql -e"exit" -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"' >> /dev/null
+        if [ "$?" == "0" ]; then
+                break
+        fi
+        sleep 2
+done
 
-echo "Wait Running Mariadb"
-sleep 4
-echo "Wait Running Mariadb"
-sleep 4
-echo "Wait Running Mariadb"
-sleep 4
-echo "Wait Running Mariadb"
-sleep 4
-echo "Wait Running Mariadb"
-sleep 4
+echo "Confirm CWDB and Setting Ciscowebkit Database"
+docker run -it --link cwdb:mysql --rm mariadb sh -c 'exec mysql -e"CREATE DATABASE ciscowebkit; GRANT ALL PRIVILEGES ON ciscowebkit.* TO \"cisco\"@\"localhost\" IDENTIFIED BY \"cisco123\"; GRANT ALL PRIVILEGES ON ciscowebkit.* TO \"cisco\"@\"%\" IDENTIFIED BY \"cisco123\";" -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
+
+echo "Show Databases"
+docker run -it --link cwdb:mysql --rm mariadb sh -c 'exec mysql -e"show databases;" -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
+
+echo "Show Container"
+docker ps
 
 docker build --rm --tag ciscowebkit . 
