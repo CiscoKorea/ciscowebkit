baeeb6b51692686216a862c79e054478fe5f266f
Hye-Churn, Jang <hyjang@cisco.com>
2016-09-13 16:37:33 +0900

    docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+docker pull mariadb
 
+docker run -d --name cwdb -e MYSQL_ROOT_PASSWORD=cisco123 -d mariadb
+
+echo "Wait Running Mariadb"
+sleep 4
+echo "Wait Running Mariadb"
+sleep 4
+echo "Wait Running Mariadb"
+sleep 4
+echo "Wait Running Mariadb"
+sleep 4
+echo "Wait Running Mariadb"
+sleep 4
+
+docker run -it --link some-mariadb:mysql --rm mariadb sh -c 'exec mysql -e "CREATE DATABASE ciscowebkit; GRANT ALL PRIVILEGES ON ciscowebkit.* TO 'cisco'@'localhost' IDENTIFIED BY 'cisco123'; GRANT ALL PRIVILEGES ON ciscowebkit.* TO 'cisco'@'%' IDENTIFIED BY 'cisco123';" -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
+
+docker build --rm --tag ciscowebkit . 
