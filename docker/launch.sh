7b8f946515bb973f1dd8cc1f8e006af13ba3365a
Hye-Churn, Jang <hyjang@cisco.com>
2016-09-19 16:14:56 +0900

    docker

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/CiscoKorea/ciscowebkit.git /opt/ciscowebkit
-python /opt/ciscowebkit/manage.py makemigrations
-python /opt/ciscowebkit/manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('cisco', 'admin@example.com', 'cisco123')" | python /opt/ciscowebkit/manage.py shell
-python /opt/ciscowebkit/manage.py runserver --settings docker.settings 0.0.0.0:80
+cd /opt/ciscowebkit && python manage.py makemigrations --settings docker.settings
+cd /opt/ciscowebkit && python manage.py migrate --settings docker.settings
+cd /opt/ciscowebkit && echo "from django.contrib.auth.models import User; User.objects.create_superuser('cisco', 'admin@example.com', 'cisco123')" | python manage.py shell --settings docker.settings
+cd /opt/ciscowebkit && python manage.py runserver --settings docker.settings 0.0.0.0:80
